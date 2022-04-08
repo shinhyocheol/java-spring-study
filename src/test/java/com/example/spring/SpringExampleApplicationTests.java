2a9f2e4e27acf518b17f9c086aeb8243d1806538
shinhyocheol <zzzskssk1@naver.com>
2022-04-08 17:56:10 +0900

    # add: 스트림 테스트 코드 작성중

--- src/test/java/com/example/spring/SpringExampleApplicationTests.java
+++ src/test/java/com/example/spring/SpringExampleApplicationTests.java
@@ -4,13 +4,16 @@ import com.example.java.bagic.EnumExample;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @SpringBootTest
 class SpringExampleApplicationTests {
 
     @Test
-    void contextLoads() {
+    public void contextLoads() throws Exception{
 
         List<EnumExample> list = EnumExample.CODES;
         int count = 0;
@@ -21,8 +24,22 @@ class SpringExampleApplicationTests {
             count = 0;
         }
 
-        System.out.println(key.getName());
+        System.out.println(key.getName());   
         System.out.println(key.getValue());
+
+    }
+
+    @Test
+    public void 스트림_사용해보기() throws Exception {
+
+        String[] stringArray = {"shinhyocheol", "hyobang", "shin"};
+        Set<String> set = Arrays.asList(stringArray)
+                .stream()
+                .filter(e -> e.startsWith("shin"))
+                .collect(Collectors.toSet());
+
+        set.forEach(System.out::println);
+
 
     }
 
